5811b6d0e8e68a97faeef255b71fa886e56f13d6
chung luong <thuanchung85@gmail.com>
2026-05-25 12:29:09 +0700

    add gitignore

--- application/build_TankGame_embedded/glcdfont.d
+++
@@ -1,2 +0,0 @@
-build_TankGame_embedded/glcdfont.o: \
- sources/driver/Adafruit_oled_drv/glcdfont.cpp
--- application/build_TankGame_embedded/hooks.d
+++
@@ -1,2 +0,0 @@
-build_TankGame_embedded/hooks.o: \
- sources/platform/stm32l/arduino/cores/stm32/hooks.c
